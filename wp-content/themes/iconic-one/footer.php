f275ed0c57ab92cdbc5ceb7f1543e8358b65e859
rolenweb <rolenweb@yandex.ru>
2016-10-13 13:04:03 +0300

    counter

--- wp-content/themes/iconic-one/footer.php
+++ wp-content/themes/iconic-one/footer.php
@@ -13,6 +13,19 @@
 		<div class="site-wordpress">
 				<?php echo date("Y").' &copy Приколы из VK'  ?>
 				</div><!-- .site-info -->
+				<div>
+					<!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='//www.liveinternet.ru/click' "+
+"target=_blank><img src='//counter.yadro.ru/hit?t44.5;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet' "+
+"border='0' width='31' height='31'><\/a>")
+//--></script><!--/LiveInternet-->
+
+				</div>
 				<div class="clear"></div>
 </div><!-- #page -->
 
