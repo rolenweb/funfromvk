a44752b0d3c2a3e17eccb83fc1845593ce74a5dd
rolenweb <rolenweb@yandex.ru>
2016-10-13 12:20:21 +0300

    set theme

---
+++ wp-content/themes/iconic-one/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Footer section template.
+ * @package WordPress
+ * @subpackage Iconic_One
+ * @since Iconic One 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		
+		</footer><!-- #colophon -->
+		<div class="site-wordpress">
+				<?php echo date("Y").' &copy Приколы из VK'  ?>
+				</div><!-- .site-info -->
+				<div class="clear"></div>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
